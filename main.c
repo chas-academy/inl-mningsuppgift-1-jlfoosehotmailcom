dcc0e9945487bb5d24d4eadfb7a23e4b419340dd
jlfoose@hotmail.com <jlfoose@hotmail.com>
2025-08-31 17:30:10 +0200

    Finished dice simulation assignment

--- main.c
+++ main.c
@@ -8,6 +8,30 @@ int main()
 
    
     // Your code goes here
+    int throws[100];
+    int counts[6] = {0};
+    int sum = 0;
+
+    // loop for 100 throws
+    for (int i = 0; i < 100; i++){
+        int roll = (rand() % 6) + 1;
+
+        throws[i] = roll;
+        counts[roll - 1]++;
+        sum += roll;
+    }
+
+    // print counts for 1-6
+    for (int j = 0; j < 6; j++){
+        printf("%d\n", counts[j]);
+    }
+
+    // print sum
+    printf("%d\n", sum);
+
+    // print average
+    float avg = (float)sum / 100;
+    printf("%.1f\n", avg);
 
     return 0;
 }
